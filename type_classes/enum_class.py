100113ad21ab9f0f357bf018540a7a48d0c64e97
nicholas-dinicola <nicholasdinicola@outlook.com>
2022-08-26 17:53:29 +0200

    Enum+Dataclass restict type

---
+++ type_classes/enum_class.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from enum import Enum
+from dataclasses import dataclass
+
+class OrgRole(Enum): 
+    MANAGER = "manager"
+    CEO = "ceo"
+    ANALYST = "analyst"
+
+@dataclass
+class Employee(object):
+    age: int
+    role: OrgRole
+
+def main() -> None:
+    nicholas = Employee(age=27, role=OrgRole.ANALYST)
+    juan = Employee(age=35, role=OrgRole.MANAGER)
+    print(nicholas)
+    print(juan)
+    # Next class will not work because role attrs its tide to the defined OrgRole
+    louis = Employee(age=35, role="CTO")
+
+if __name__ == "__main__": 
+    main()
+
