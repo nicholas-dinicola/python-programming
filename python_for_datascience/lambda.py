644ebf74369b566ffa7dab0338778065a83718de
nicholas-dinicola <nicholasdinicola@outlook.com>
2023-02-26 17:44:47 +0100

    Lambda with map and filter

--- python_for_datascience/lambda.py
+++ python_for_datascience/lambda.py
@@ -1,1 +1,10 @@
+numbers = [1, 2, 3, 4, 5]
 
+# Use map() and a lambda function to square numbers
+squared = map(lambda x: x**2, numbers)
+print(list(squared))
+
+# Use filter() and a lambda function to filter out odd numbers
+even_numbers = list(filter(lambda x: x % 2==0, numbers))
+
+print(even_numbers)
